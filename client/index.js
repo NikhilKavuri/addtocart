ad34f329a0cc7b8a00051cde88fb2a2fbd93a90d
Nikhil Kavuri <nikhilkavuri1503@gmail.com>
2023-08-07 14:37:46 +0530

    Updated css and JS
    Updated Css in alphabetical order and JS with comments

--- client/index.js
+++ client/index.js
@@ -8,7 +8,9 @@ const addToCart = document.getElementsByClassName("add-btn")[0];
 const showToast = document.getElementsByClassName("success-toast-container")[0];
 
 for (let i = 0; i < checkBoxContainers.length; i++) {
+  // Event listener for the container click
   checkBoxContainers[i].addEventListener("click", () => {
+    // if nothing is clicked
     if (activeContainerIndex !== -1 && activeContainerIndex !== i) {
       checkBoxContainers[activeContainerIndex].classList.remove("active");
       checkBoxContainers[activeContainerIndex].style.backgroundColor =
@@ -18,6 +20,7 @@ for (let i = 0; i < checkBoxContainers.length; i++) {
       originalPrices[activeContainerIndex].style.display = "none";
       radioInputs[activeContainerIndex].checked = false;
     }
+    // if a pair is selected
     if (activeContainerIndex !== i) {
       checkBoxContainers[i].classList.add("active");
       checkBoxContainers[i].style.backgroundColor = "#F4FBF9";
@@ -27,7 +30,9 @@ for (let i = 0; i < checkBoxContainers.length; i++) {
       populars[i].style.display = "block";
       radioInputs[i].checked = true;
       activeContainerIndex = i;
-    } else {
+    }
+    // if the second one is selected then close the before one 
+    else {
       checkBoxContainers[i].classList.remove("active");
       checkBoxContainers[i].style.backgroundColor = "initial";
       tables[i].style.display = "none";
@@ -37,6 +42,7 @@ for (let i = 0; i < checkBoxContainers.length; i++) {
       activeContainerIndex = -1;
     }
   });
+  // To stop event bubbling while selecting the Table
   let tablesInContainer = checkBoxContainers[i].querySelectorAll("table");
   for (let j = 0; j < tablesInContainer.length; j++) {
     tablesInContainer[j].addEventListener("click", (event) => {
@@ -44,14 +50,18 @@ for (let i = 0; i < checkBoxContainers.length; i++) {
     });
   }
 }
+// Event Listener to add to cart
 addToCart.addEventListener("click", () => {
+  // If any of the pair is selected then Toast should be success
   if (activeContainerIndex >= 0) {
     showToast.firstElementChild.textContent = "Added to Cart Successfully";
     showToast.style.backgroundColor = "rgb(169, 243, 171)";
     showToast.firstElementChild.style.color = "#3c8956";
     showToast.firstElementChild.style.textAlign = "center";
     showToast.style.display = "block";
-  } else {
+  }
+  // If Nothing is selected then Toast should be Error 
+  else {
     showToast.firstElementChild.textContent = "Please select something to add";
     showToast.firstElementChild.style.color = "white";
     showToast.style.backgroundColor = "#F45050";
@@ -60,6 +70,7 @@ addToCart.addEventListener("click", () => {
     showToast.style.left = "39vw";
     showToast.style.display = "block";
   }
+  // remove the toast message after 2 seconds
   setTimeout(() => {
     showToast.style.display = "none";
   }, 2000);
